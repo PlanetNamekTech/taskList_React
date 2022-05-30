b8a2e4ce3caca52727c7476cdb5b9e45b7bc076d
PlanetNamekTech <sr23513@gmail.com>
2022-05-30 09:37:36 -0700

    Fully functional UI

--- task-tracker-react/src/App.js
+++ task-tracker-react/src/App.js
@@ -1,9 +1,12 @@
-import Header from './components/Header'
-import Tasks from './components/Tasks'
 import { useState } from 'react'
 // State for dynamic data
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 
 const App = ()  => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -27,6 +30,14 @@ const App = ()  => {
     ]
 )
 
+// Add Task
+const addTask = (task) => {
+  // Multiple ways to add to state
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 // Delete a Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==id))
@@ -39,7 +50,8 @@ const toggleReminder = (id) => {
 
   return ( // Below is all JSX
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'Empty List'}
     </div>
   );
--- task-tracker-react/src/components/Header.js
+++ task-tracker-react/src/components/Header.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = (object) => {
-    console.log(object)
-  }
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      {/* Dynamic pages without reloading */}
+      <Button color={showAdd ? 'Black' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
